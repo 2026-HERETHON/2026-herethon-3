9fd5cdad0dcf17b8b6197c81ee53ed6cf3971a7b
bomee1123 <bomee1123@ewha.ac.kr>
2026-07-08 16:10:45 +0900

    accounts 수정 및 reviews 앱 구성

--- grids/admin.py
+++ grids/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Grid
+
+
+@admin.register(Grid)
+class GridAdmin(admin.ModelAdmin):
+    list_display = ('id', 'dong', 'latitude', 'longitude', 'safety_score')
+    search_fields = ('dong',)
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Review, ReviewLike
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'grid', 'rating_night', 'rating_amenity', 'rating_mood', 'like_count', 'created_at')
+    list_filter = ('grid',)
+    search_fields = ('review_content', 'user__nickname')
+
+
+@admin.register(ReviewLike)
+class ReviewLikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'review')
