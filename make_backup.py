739f05b65a0507c251bc9bf1249319726a63bd81
benumarsonbush <syfaceking@gmail.com>
2026-07-12 20:55:04 +0900

    Chore: 최상위 .gitignore 경로 수정 및 FE 규칙 추가

--- make_backup.py
+++
@@ -1,9 +0,0 @@
-import json
-
-backup_data = {
-    "상계동": {"summary_raw": {"night_safety_density": 0.2910134060440809, "crime_zone_density": 0.17124719295215063}},
-    "신림동": {"summary_raw": {"night_safety_density": 0.20117032155436881, "crime_zone_density": 0.20053299492385784}},
-}
-with open("data/safety_scores_backup_wms_ok.json", "w", encoding="utf-8") as f:
-    json.dump(backup_data, f, ensure_ascii=False, indent=2)
-    
