8b5a845b2e06a86ad4cd832ef78b43bc508169fe
kimgyurin <koong5111@naver.com>
2026-07-13 18:38:03 +0900

    feat(grids): 실거주지 인증용 좌표 판정 엔드포인트 추가

--- backend/grids/urls.py
+++ backend/grids/urls.py
@@ -6,5 +6,6 @@ app_name = 'grids'
 urlpatterns = [
     path('', views.grid_list, name='list'),
     path('facilities/', views.facility_list, name='facilities'),  
+    path('verify-location/', views.verify_location, name='verify_location'),  # 추가
     path('<str:dong>/', views.grid_detail, name='detail'),        
 ]
