6a40010015f421f6d627f53d3d9be4325a6c1adb
kimgyurin <koong5111@naver.com>
2026-07-14 23:39:35 +0900

    settings.py 수정

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -48,12 +48,14 @@ INSTALLED_APPS = [
     "reviews.apps.ReviewsConfig",
     "grids.apps.GridsConfig",
     "qna.apps.QnaConfig",
+    'corsheaders',
     
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware', 
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -109,7 +111,12 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
 
+CORS_ALLOW_CREDENTIALS = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
